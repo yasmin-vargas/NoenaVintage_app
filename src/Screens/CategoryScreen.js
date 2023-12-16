71e293371a713f732c8ec1ddb711f2ccc05dad5f
NoenaVintage <ni_varro@yahoo.es>
2023-12-16 20:32:47 +0100

    Added Filters

--- src/Screens/CategoryScreen.js
+++ src/Screens/CategoryScreen.js
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, Text, TouchableOpacity, CheckBox } from 'react-native';
 import axios from 'axios';
+
+// Component to display and handle attribute filters
+function AttributeFilters({ attributeTypes, selectedAttributes, onSelect }) {
+    return (
+        <ScrollView horizontal>
+            {attributeTypes.map((attributeType) => (
+                <CheckBox
+                    key={attributeType}
+                    title={attributeType}
+                    checked={selectedAttributes.includes(attributeType)}
+                    onPress={() => onSelect(attributeType)}
+                />
+            ))}
+        </ScrollView>
+    );
+}
 
 const CategoryScreen = ({ route, navigation }) => {
     const { category } = route.params;
@@ -16,14 +32,13 @@ const CategoryScreen = ({ route, navigation }) => {
             .catch(error => {
                 console.error(`Error fetching products for ${category}:`, error);
             });
-
-        // Fetch attributes
-        fetchAttributes();
+        fetchAttributes();  // Fetch attributes
     }, [category]);
+
 
     const fetchAttributes = async () => {
         try {
-            const response = await axios.get('https://noenavintagedk.ew.r.appspot.com/attributes');
+            const response = await axios.get('https://noenavintagedk.ew.r.appspot.com/attributes/getAllAttributes');
             setAttributes(response.data); // assuming the response is an array of attributes
         } catch (error) {
             console.error('Error fetching attributes:', error);
@@ -32,45 +47,33 @@ const CategoryScreen = ({ route, navigation }) => {
 
     const handleCategoryPress = (category) => {
         // Navigate to ProductScreen
-        navigation.navigate('ProductScreen', { Category });
+        navigation.navigate('ProductScreen', { category });
     };
 
-    const handleAttributeToggle = (attributeID) => {
-        // Toggle the selected attribute
-        setSelectedAttributes((prevSelected) => {
-            const isSelected = prevSelected.includes(attributeID);
-            if (isSelected) {
-                // Attribute is already selected, remove it
-                return prevSelected.filter((id) => id !== attributeID);
-            } else {
-                // Attribute is not selected, add it
-                return [...prevSelected, attributeID];
-            }
-        });
+    const handleAttributeToggle = (attributeType) => {
+        setSelectedAttributes((prevSelected) => (
+            prevSelected.includes(attributeType)
+                ? prevSelected.filter((type) => type !== attributeType)
+                : [...prevSelected, attributeType]
+        ));
     };
 
     // Apply filtering based on selected attributes
-    const filteredProducts = products.filter((product) => {
-        // Check if the product has all selected attributes
-        return selectedAttributes.every((attributeId) => {
-            return product.attributeIDs.includes(attributeId);
-        });
-    });
+    const filteredProducts = products.filter((product) => (
+        selectedAttributes.every((attributeType) => (
+            product.attributes.some((attribute) => attribute.attributeType === attributeType)
+        ))
+    ));
 
     return (
         <ScrollView>
             <Text>{category}</Text>
             <Text h4>Filter by Attributes</Text>
-            {attributes.map((attribute) => (
-                <CheckBox
-                    key={attribute.attributeID}
-                    title={attribute.attributeValue} // Adjust according to your attribute structure
-                    checked={selectedAttributes.includes(attribute.attributeID)}
-                    onPress={() => handleAttributeToggle(attribute.attributeID)}
-                />
-            ))}
-
-            {/* Map over the fetched and filtered products */}
+            <AttributeFilters
+                attributeTypes={['Brand', 'Decade', 'Condition', 'Style', 'Material', 'Print', 'Colour', 'Size', 'Price']}
+                selectedAttributes={selectedAttributes}
+                onSelect={handleAttributeToggle}
+            />
             {filteredProducts.map((product) => (
                 <TouchableOpacity key={product.id} onPress={() => handleProductPress(product)}>
                     <Text>{product.name}</Text>
